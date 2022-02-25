ce2f4de839ce71c1f0361e53b8aa83a20541aecd
vitmonjo <vitmonjo@gmail.com>
2022-02-25 15:15:19 -0300

    Added logic for planet fetching

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
+// People fetch
+// let peopleArray = [];
 
-let peopleArray = [];
+// let peopleRequest = `https://swapi.dev/api/people/`;
+// fetch(peopleRequest).then((response) => {
+//     return response.json();
+// }).then((data) => {
+//     peopleArray = peopleArray.concat(data.results);
+// })
 
-let peopleRequest = `https://swapi.dev/api/people/`;
-fetch(peopleRequest).then((response) => {
+// for (let i = 2; i < 10; i++) {
+//     let peopleRequest = `https://swapi.dev/api/people/?page=${i}`;
+//     fetch(peopleRequest).then((response) => {
+//         return response.json();
+//     }).then((data) => {
+//         peopleArray = peopleArray.concat(data.results);
+//     })
+// }
+
+// Planets fetch
+let planetsArray = [];
+
+let request = `https://swapi.dev/api/planets/`;
+fetch(request).then((response) => {
     return response.json();
 }).then((data) => {
-    peopleArray = peopleArray.concat(data.results);
+    planetsArray = planetsArray.concat(data.results);
 })
 
-for (let i = 2; i < 10; i++) {
-    let peopleRequest = `https://swapi.dev/api/people/?page=${i}`;
-    fetch(peopleRequest).then((response) => {
+for (let i = 2; i < 7; i++) {
+    let request = `https://swapi.dev/api/planets/?page=${i}`;
+    fetch(request).then((response) => {
         return response.json();
     }).then((data) => {
-        peopleArray = peopleArray.concat(data.results);
+        planetsArray = planetsArray.concat(data.results);
     })
 }
-
